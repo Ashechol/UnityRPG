e4a33680ca644236872d89d81d101d00c560cd0d
ashechol <ashechol@qq.com>
2022-04-13 00:04:58 +0800

    update

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -268,8 +268,8 @@ public class EnemyController : MonoBehaviour, IEndGameObserver
         // Gizmos.color = Color.green;
         // Gizmos.DrawWireSphere(transform.position, sightRadius);
         // 绘制视野扇形
-        GizmosEx.DrawWireArc(transform, sightRadius, sightAngle, Color.green);
-        GizmosEx.DrawWireArc(transform, sightRadius, chaseSightAngle, Color.red);
+        GizmosEx.DrawWireArc(transform, sightRadius, sightAngle, Color.green, 50, 1);
+        // GizmosEx.DrawWireArc(transform, sightRadius, chaseSightAngle, Color.red);
     }
 
     // Animation Event
--- Assets/Scripts/Tools/GizmosExtension.cs
+++ Assets/Scripts/Tools/GizmosExtension.cs
@@ -5,22 +5,28 @@ namespace Utils
     public class GizmosEx
     {
         public static void DrawWireArc(Transform origin, float radius, float angle, Color color,
-                                       int segments = 50)
+                                       int segments = 50, float deviation = 0)
         {
             Gizmos.color = color;
-            Vector3 dir = Quaternion.AngleAxis(angle, origin.up) * origin.forward * radius;
+            Vector3 offset = Vector3.up * deviation;
+            Vector3 dir = Quaternion.AngleAxis(angle, origin.up) * origin.forward * radius + offset;
 
             float mdeg = angle * 2 / segments;
             Vector3 currentPos = origin.position + dir;
             Vector3 oldPos;
-            Gizmos.DrawLine(origin.position, currentPos);
+
+            if (angle != 180)
+                Gizmos.DrawLine(origin.position + offset, currentPos);
+
             for (int i = 0; i <= segments; i++)
             {
                 oldPos = currentPos;
                 currentPos = origin.position + Quaternion.AngleAxis(-i * mdeg, origin.up) * dir;
                 Gizmos.DrawLine(oldPos, currentPos);
             }
-            Gizmos.DrawLine(origin.position, currentPos);
+
+            if (angle != 180)
+                Gizmos.DrawLine(origin.position + offset, currentPos);
 
         }
     }
