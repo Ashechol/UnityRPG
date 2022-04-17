19c0f9d1aaabde435d2eeab897b8ed4a8384fc3d
ashechol <ashechol@qq.com>
2022-04-17 21:47:13 +0800

    Adjust method in SceneLoadManager.cs

--- Assets/Scripts/Managers/SceneLoadManager.cs
+++ Assets/Scripts/Managers/SceneLoadManager.cs
@@ -10,6 +10,7 @@ public class SceneLoadManager : Singleton<SceneLoadManager>
     GameObject player;
     NavMeshAgent playerAgent;
     Transform dst;
+    bool loadingComplete;
     public bool canTeleport;
 
     protected override void Awake()
@@ -22,28 +23,25 @@ public class SceneLoadManager : Singleton<SceneLoadManager>
         switch (portal.transitionType)
         {
             case Portal.TransitionType.SameScene:
-                StartCoroutine(Teleport(portal.dstTag));
+                Teleport(portal.dstTag);
                 break;
             case Portal.TransitionType.DifferentScene:
-                StartCoroutine(Teleport(portal.dstTag, portal.sceneName));
+                Teleport(portal.dstTag, portal.sceneName);
                 break;
         }
     }
 
-    IEnumerator Teleport(Portal.PortalTag dstTag, string sceneName = null)
+    void Teleport(Portal.PortalTag dstTag, string sceneName = null)
     {
-        //保存数据
-
-        if (sceneName != null)
+        if (sceneName != null)  // 不同关卡传送
         {
             SaveManager.Instance.SavePlayerData();
-            yield return SceneManager.LoadSceneAsync(sceneName);
-            dst = GetDestination(dstTag);
-            yield return Instantiate(playerPrefab, dst.position, dst.rotation);
-            SaveManager.Instance.LoadPlayerData();
-            yield break;
+            StartCoroutine(LoadScene(sceneName, dstTag));
+            if (loadingComplete)
+                SaveManager.Instance.LoadPlayerData();
+            loadingComplete = false;
         }
-        else
+        else  // 同关卡传送
         {
             player = GameManager.Instance.playerStats.gameObject;
             playerAgent = player.GetComponent<NavMeshAgent>();
@@ -52,7 +50,6 @@ public class SceneLoadManager : Singleton<SceneLoadManager>
             playerAgent.enabled = false;
             player.transform.SetPositionAndRotation(dst.position, dst.rotation);
             playerAgent.enabled = true;
-            yield return null;
         }
     }
 
@@ -62,22 +59,9 @@ public class SceneLoadManager : Singleton<SceneLoadManager>
         dst = GetDestination(dstTag);
 
         if (sceneName != "Main Menu")
-        {
             yield return Instantiate(playerPrefab, dst.position, dst.rotation);
-            SaveManager.Instance.SavePlayerData();
-        }
-        yield break;
-    }
 
-    IEnumerator LoadSavedScene(string sceneName, Portal.PortalTag dstTag = Portal.PortalTag.ENTER)
-    {
-        yield return SceneManager.LoadSceneAsync(sceneName);
-
-        dst = GetDestination(dstTag);
-
-        yield return Instantiate(playerPrefab, dst.position, dst.rotation);
-
-        SaveManager.Instance.LoadPlayerData();
+        loadingComplete = true;  // 场景和角色加载完成
         yield break;
     }
 
@@ -89,11 +73,17 @@ public class SceneLoadManager : Singleton<SceneLoadManager>
     public void LoadFirstLevel()
     {
         StartCoroutine(LoadScene("Big Plain"));
+        if (loadingComplete)
+            SaveManager.Instance.SavePlayerData();
+        loadingComplete = false;
     }
 
     public void ContinueScene()
     {
-        StartCoroutine(LoadSavedScene(SaveManager.Instance.SavedScene));
+        StartCoroutine(LoadScene(SaveManager.Instance.SavedScene));
+        if (loadingComplete)
+            SaveManager.Instance.LoadPlayerData();
+        loadingComplete = false;
     }
 
     private Transform GetDestination(Portal.PortalTag dstTag)
